4ad97a94185c58adf0b31f59e6da627f3d0f0061
FabioPin <fajos.pinheiro@hotmail.com>
2023-10-27 23:34:10 +0100

    formData way initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    
+    const email = formData.get('email');
+
+    console.log(email)
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName" //Important
+          placeholder="First Name"
+        />
+        <br />
+        <input
+          type="email"
+          name="email" //Important
+          placeholder="Email Adress"
+        />
+        <br />
+        <input
+          type="tel"
+          name="phone" //Important
+          placeholder="Phone Number"
+        />
+        <br />
+        <input
+          name="company" //Important
+          placeholder="Company Name"
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
 
 export default App
